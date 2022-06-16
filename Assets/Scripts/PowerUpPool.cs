4bffbeaf6f29bcc1da8da9c88a37ab3d2967a17f
Thomas Nguyen Ly <34043095+ThomasNLy@users.noreply.github.com>
2022-06-16 13:52:38 -0400

    Create PowerUpPool.cs
    inherits from object pool script

---
+++ Assets/Scripts/PowerUpPool.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpPool : ObjectPoolSpawn
+{
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        spawnRate = 5;
+        SetUp();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnObjects(); 
+    }
+
+    public override void SpawnObjects()
+    {
+        if (spawnIndex >= objects.Length)
+        {
+            spawnIndex = 0;
+        }
+
+        if (objects[spawnIndex].transform.position.x <= outOfBoundsBarrier.transform.position.x && spawnTimer > spawnRate)
+        {
+            Debug.Log("spawn");
+            float randomY = Random.Range(spawnLoc.transform.position.y, spawnLoc.transform.position.y + 7);
+            objects[spawnIndex].transform.position = new Vector2(spawnLoc.transform.position.x, randomY);
+            spawnTimer = 0;
+        }
+        spawnIndex++;
+        spawnTimer += Time.deltaTime;
+    }
+}
