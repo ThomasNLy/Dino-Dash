996265cf23ec58ca3177bc5611552c63c49ee324
Thomas Nguyen Ly <34043095+ThomasNLy@users.noreply.github.com>
2022-06-16 13:51:31 -0400

    made it class that can be inherited from

--- Assets/Scripts/ObjectPoolSpawn.cs
+++ Assets/Scripts/ObjectPoolSpawn.cs
@@ -5,44 +5,57 @@ using UnityEngine;
 public class ObjectPoolSpawn : MonoBehaviour
 {
     // Start is called before the first frame update
-    GameObject [] objects;
-    int numOfObjects = 6;
+    protected GameObject [] objects;
+    protected int numOfObjects = 6;
     public GameObject prefab;
     public GameObject spawnLoc;
     public GameObject outOfBoundsBarrier;
-    int spawnIndex = 0;
-    float spawnTimer = 0;
+    protected int spawnIndex = 0;
+    protected float spawnTimer = 0;
+    public float spawnRate = 3;
     void Start()
+    {
+        SetUp();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnObjects();
+    }
+
+    /**
+     * needed for inheritance as a parent class
+     */
+    public void SetUp()
     {
         outOfBoundsBarrier = GameObject.Find("OutOfBoundsBarrier");
         spawnLoc = GameObject.FindGameObjectWithTag("SpawnLoc");
-        
+
         objects = new GameObject[numOfObjects];
         for (int i = 0; i < numOfObjects; i++)
         {
             objects[i] = Instantiate(prefab, new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
         }
     }
-
-    // Update is called once per frame
-    void Update()
-    {   
-        
+    public virtual void SpawnObjects()
+    {
         if (spawnIndex >= objects.Length)
         {
             spawnIndex = 0;
-           
+
         }
-  
-        if (spawnTimer > 3 && objects[spawnIndex].transform.position.x <= outOfBoundsBarrier.transform.position.x)
+
+        if (spawnTimer > spawnRate && objects[spawnIndex].transform.position.x <= outOfBoundsBarrier.transform.position.x)
         {
-         
+
             objects[spawnIndex].transform.position = spawnLoc.transform.position;
             spawnTimer = 0;
         }
 
         spawnIndex++;
         spawnTimer += Time.deltaTime;
-
     }
 }
+
+
