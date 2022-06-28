3566cb60da0159a989aa2dcade3a85a1c7f97897
Thomas Nguyen Ly <34043095+ThomasNLy@users.noreply.github.com>
2022-06-27 21:13:43 -0400

    script is now attached to the individual power ups to prevent bug of player picking them up when hit by attack beam

--- Assets/Scripts/PlayerCollectPowerUp.cs
+++ Assets/Scripts/PlayerCollectPowerUp.cs
@@ -4,21 +4,10 @@ using UnityEngine;
 
 public class PlayerCollectPowerUp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "PowerUp")
+        if (collision.gameObject.name == "Player")
         {
             GameController.Instance.IncreasePowerUpBar();
         }
