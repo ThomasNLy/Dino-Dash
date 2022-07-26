6176d1e5f571936956343fa5a8246468ec6185ef
Thomas Nguyen Ly <34043095+ThomasNLy@users.noreply.github.com>
2022-07-25 21:36:36 -0400

    Moved movement code to fixed update to work with physics engine better

--- Assets/Scripts/ScrollingObject.cs
+++ Assets/Scripts/ScrollingObject.cs
@@ -7,24 +7,39 @@ public class ScrollingObject : MonoBehaviour
 
     Rigidbody2D rb;
     [SerializeField]
-    private float speed = -3.5f;
+    private float speed = -0.06f;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        speed = -3.25f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (GameController.Instance.IsGameOver() == false)
+        //if (GameController.Instance.IsGameOver() == false && GameController.Instance.IsPaused() == false)
+        //{
+        //    rb.velocity = new Vector2(speed, 0);
+           
+        //}
+        //else 
+        //{
+        //    rb.velocity = Vector2.zero;
+        //}
+           
+    }
+
+    private void FixedUpdate()
+    {
+        if (GameController.Instance.IsGameOver() == false && GameController.Instance.IsPaused() == false)
         {
             rb.velocity = new Vector2(speed, 0);
+
         }
-        else 
+        else
         {
             rb.velocity = Vector2.zero;
         }
-           
     }
 }
