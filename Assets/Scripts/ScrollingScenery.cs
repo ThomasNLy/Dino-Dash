20ca148fefd8e694c156a907fe91f730c8128536
Thomas Nguyen Ly <34043095+ThomasNLy@users.noreply.github.com>
2022-07-25 22:10:10 -0400

    Updated to act as a parent class for all scrolling scenery objects
    Will be used as a base class for having various objects in the scene scrolling as a part of the scenery.

--- Assets/Scripts/ScrollingScenery.cs
+++ Assets/Scripts/ScrollingScenery.cs
@@ -1,37 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
+
 
 public class ScrollingScenery : MonoBehaviour
 {
-    public float scrollSpeed;
-    int tileMapSize;
-    public GameObject otherFloor;
-    int otherTileMapSize;
+    [SerializeField]
+    protected float scrollSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        otherTileMapSize = otherFloor.transform.GetChild(0).GetComponent<Tilemap>().size.x;
-        tileMapSize = gameObject.transform.GetChild(0).GetComponent<Tilemap>().size.x;
+       
         scrollSpeed = -3.25f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Scroll();
+           
+    }
+
+    public void Scroll()
+    {
         if (GameController.Instance.IsGameOver() == false && GameController.Instance.IsPaused() == false)
         {
             gameObject.transform.Translate(new Vector3(scrollSpeed, 0) * Time.deltaTime);
-            if (gameObject.transform.position.x + tileMapSize <= -9.5)
-            {
-                gameObject.transform.position = new Vector3(otherFloor.transform.position.x + otherTileMapSize - 1, otherFloor.transform.position.y);
-            }
+            wrapAround();
         }
-        else 
+        else
         {
             gameObject.transform.Translate(new Vector3(0, 0));
         }
-           
+    }
+
+    public  virtual void wrapAround()
+    {
+        if (gameObject.transform.position.x <= GameController.Instance.outOfBoundsBarrier.transform.position.x)
+        {
+            gameObject.transform.position = new Vector3(GameController.Instance.spawnLoc.transform.position.x, GameController.Instance.spawnLoc.transform.position.y);
+        }
     }
 }
