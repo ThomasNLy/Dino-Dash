9733ef914a8a16f0ad826db30080a33ee80fb179
Thomas Nguyen Ly <34043095+ThomasNLy@users.noreply.github.com>
2022-08-06 12:05:42 -0400

    Random spawn and run across screen animation done
    Had the dino spawn at a random location, pop up than run across the screen. Further more fixed the issue with the sprite not facing the direction by having it flipped based on the directions the dino is running.

--- Assets/Scripts/MainMenuScripts/DinoAnimationController.cs
+++ Assets/Scripts/MainMenuScripts/DinoAnimationController.cs
@@ -7,12 +7,18 @@ public class DinoAnimationController : MonoBehaviour
     public GameObject spawnLoc1;
     public GameObject spawnLoc2;
     public GameObject spawnLoc3;
-    public GameObject runSpawnLoc;
-    public GameObject outOfBounds;
+    private GameObject[] spawnLocations;
+    public GameObject runSpawnLoc1;
+    public GameObject runSpawnLoc2;
+    private GameObject[] runSpawnLocations;
+    public GameObject outOfBoundsBarrier1;
+    public GameObject outOfBoundsBarrier2;
     public GameObject parent;
     public Animator animatingDino;
-    GameObject[] spawnLocations;
-    float timer = 0f;
+    SpriteRenderer spriteRenderer;
+
+  
+    private float timer = 0f;
     private const int POPUPANIMDONE = 1;
     private const int RUNANIM = 2;
     private const int RUNANIMDONE = 3;
@@ -20,7 +26,9 @@ public class DinoAnimationController : MonoBehaviour
     [SerializeField]
     private int animState = 0;
     [SerializeField]
-    bool animating = false;
+    private bool animating = false;
+    private bool faceRight = true;
+    private int runSpeed = 5;
 
     // Start is called before the first frame update
     void Start()
@@ -29,66 +37,108 @@ public class DinoAnimationController : MonoBehaviour
         spawnLocations[0] = spawnLoc1;
         spawnLocations[1] = spawnLoc2;
         spawnLocations[2] = spawnLoc3;
-        parent.transform.position = spawnLocations[0].transform.position; 
+        parent.transform.position = spawnLocations[0].transform.position;
+
+        runSpawnLocations = new GameObject[2];
+        runSpawnLocations[0] = runSpawnLoc1;
+        runSpawnLocations[1] = runSpawnLoc2;
+
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+
         timer += Time.deltaTime;
-        if (timer > 10f && animating == false)
+        if (timer > 2f && animating == false)
         {
-            Debug.Log("random spawn");
+
             int randomLoc = (int)Random.Range(0, 3);
-            parent.transform.position =  spawnLocations[randomLoc].transform.position; // a parent is needed to move the game object around as animating the position directly is based on world coordinates, not local coordinates. 
+            parent.transform.position = spawnLocations[randomLoc].transform.position; // a parent is needed to move the game object around as animating the position directly is based on world coordinates, not local coordinates. 
             gameObject.transform.localPosition = new Vector2(0, 0); //needed to reset it back to the center of the parent gameObject, the animation can cause the dino to sink into the ground
             animating = true;
             animatingDino.SetBool("popUp", true);
         }
 
-       
+
 
         if (animState == POPUPANIMDONE)
         {
             animatingDino.SetBool("popUp", false);
             animatingDino.SetBool("isRunning", true);
-            Debug.Log("Pop up done");
-            parent.transform.position = runSpawnLoc.transform.position;
+
+            int randomRunSpawnLoc = (int)Random.Range(0, 2);
+            parent.transform.position = runSpawnLocations[randomRunSpawnLoc].transform.position;
+            if (randomRunSpawnLoc == 0)
+            {
+                faceRight = true;
+            }
+            else
+            {
+                faceRight = false;
+            }
             animState = RUNANIM;
 
         }
         else if (animState == RUNANIM)
         {
-            parent.GetComponent<Rigidbody2D>().velocity = new Vector2(5, 0);
-            
+            Move();
         }
 
         else if (animState == RUNANIMDONE)
         {
             animatingDino.SetBool("isRunning", false);
             timer = 0f;
-            Debug.Log("run done");
-            parent.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
 
             animating = false;
             animState = IDLE;
         }
 
-        MyCollisionDetection2D(outOfBounds.GetComponent<BoxCollider2D>());
+
 
 
     }
 
+    /**
+     * use late update to apply any tranformations to the sprite renderer as the animations that affect the sprite renderer with transformations take priority and will override any changes made by script
+     * have it flip the direction of the sprite based on direction it is going
+     */
+    private void LateUpdate()
+    {
+
+        if (parent.GetComponent<Rigidbody2D>().velocity.x == runSpeed)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (parent.GetComponent<Rigidbody2D>().velocity.x == -runSpeed)
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
 
 
+
+    /**
+     * custom collision function as the parent object has the box collider instead of the child.
+     */
     private void MyCollisionDetection2D(Collider2D collider)
     {
         if (parent.GetComponent<BoxCollider2D>().IsTouching(collider))
         {
             RunAnimDone();
-            parent.transform.position -= new Vector3(5, 0, 0) * Time.deltaTime;
+            Vector3 moveBack;
+            if (faceRight)
+            {
+                moveBack = new Vector3(-5, 0, 0);
+            }
+            else
+            {
+                moveBack = new Vector3(5, 0, 0) * Time.deltaTime;
+            }
+            parent.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            parent.transform.position += moveBack * Time.deltaTime;
         }
     }
 
@@ -100,12 +150,27 @@ public class DinoAnimationController : MonoBehaviour
     public void RunAnimDone()
     {
         animState = RUNANIMDONE;
-        Debug.Log("done running");
+
     }
 
     public void RunAnim()
     {
         animState = RUNANIM;
+    }
+
+    private void Move()
+    {
+        if (faceRight)
+        {
+            parent.GetComponent<Rigidbody2D>().velocity = Vector2.right * runSpeed;
+            MyCollisionDetection2D(outOfBoundsBarrier1.GetComponent<BoxCollider2D>());
+        }
+        else
+        {
+            parent.GetComponent<Rigidbody2D>().velocity = Vector2.left * runSpeed;
+            MyCollisionDetection2D(outOfBoundsBarrier2.GetComponent<BoxCollider2D>());
+        }
+
     }
 
 }
