1b38bf62bd844b1d92b7c9b98823297a799d4a7c
Thomas Nguyen <34043095+ThomasNLy@users.noreply.github.com>
2022-08-23 18:39:39 -0400

    Added an exit  button to quit the game

--- Assets/Scripts/MainMenuScripts/MenuController.cs
+++ Assets/Scripts/MainMenuScripts/MenuController.cs
@@ -10,18 +10,21 @@ public class MenuController : MonoBehaviour
     public Button playBtn;
     public Button controlBtn;
     public Button backBtn;
+    public Button exitBtn;
     public GameObject controlMenu;
     // Start is called before the first frame update
     void Start()
     {
         sceneLoad = gameObject.GetComponent<SceneLoader>();
         playBtn = playBtn.GetComponent<Button>();
-        playBtn.onClick.AddListener(playGame);
+        playBtn.onClick.AddListener(PlayGame);
         controlBtn = controlBtn.GetComponent<Button>();
-        controlBtn.onClick.AddListener(showControlMenu);
+        controlBtn.onClick.AddListener(ShowControlMenu);
         controlMenu.SetActive(false);
         backBtn = backBtn.GetComponent<Button>();
-        backBtn.onClick.AddListener(closeControlMenu);
+        backBtn.onClick.AddListener(CloseControlMenu);
+        exitBtn = exitBtn.GetComponent<Button>();
+        exitBtn.onClick.AddListener(ExitGame);
         
     }
 
@@ -31,17 +34,22 @@ public class MenuController : MonoBehaviour
         
     }
 
-    void playGame()
+    void PlayGame()
     {
         sceneLoad.LoadScene(levelSelected);
     }
 
-    void showControlMenu()
+    void ShowControlMenu()
     {
         controlMenu.SetActive(true);
     }
-    void closeControlMenu()
+    void CloseControlMenu()
     {
         controlMenu.SetActive(false);
+    }
+
+    void ExitGame()
+    {
+        Application.Quit();
     }
 }
