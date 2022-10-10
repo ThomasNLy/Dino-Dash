2f1a94afb3626e5fe558755bf4a767604b9f686a
Thomas Nguyen <34043095+ThomasNLy@users.noreply.github.com>
2022-10-10 13:31:51 -0400

    Added buttons for settings menu
    Added a settings menu button that will display the settings menu for changing volume levels, as well as a back button to exit out of the settings menu.

--- Assets/Scripts/MainMenuScripts/MenuController.cs
+++ Assets/Scripts/MainMenuScripts/MenuController.cs
@@ -7,11 +7,16 @@ public class MenuController : MonoBehaviour
 {
     public string levelSelected;
     public SceneLoader sceneLoad;
+    [Header("Menus variables")]
     public Button playBtn;
-    public Button controlBtn;
-    public Button backBtn;
     public Button exitBtn;
     public GameObject controlMenu;
+    public Button controlBtn;
+    public Button controlMenuBackBtn;
+    [Header("Settings Menu variables")]
+    public GameObject settingsMenu;
+    public Button settingsMenuBackBtn;
+    public Button settingsBtn;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,10 +26,15 @@ public class MenuController : MonoBehaviour
         controlBtn = controlBtn.GetComponent<Button>();
         controlBtn.onClick.AddListener(ShowControlMenu);
         controlMenu.SetActive(false);
-        backBtn = backBtn.GetComponent<Button>();
-        backBtn.onClick.AddListener(CloseControlMenu);
+        controlMenuBackBtn = controlMenuBackBtn.GetComponent<Button>();
+        controlMenuBackBtn.onClick.AddListener(CloseControlMenu);
         exitBtn = exitBtn.GetComponent<Button>();
         exitBtn.onClick.AddListener(ExitGame);
+
+        //---------settings menu-----------
+        settingsMenu.SetActive(false);
+        settingsMenuBackBtn.onClick.AddListener(CloseSettingsMenu);
+        settingsBtn.onClick.AddListener(ShowSettingsMenu);
         
     }
 
@@ -46,6 +56,14 @@ public class MenuController : MonoBehaviour
     void CloseControlMenu()
     {
         controlMenu.SetActive(false);
+    }
+    void ShowSettingsMenu()
+    {
+        settingsMenu.SetActive(true);
+    }
+    void CloseSettingsMenu()
+    {
+        settingsMenu.SetActive(false);
     }
 
     void ExitGame()
